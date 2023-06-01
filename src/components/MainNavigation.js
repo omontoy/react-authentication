670961131a3686bedf139521804580804e219b53
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-06-01 13:24:12 -0500

    Use token to render UI conditionally

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,9 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
+  const token = useRouteLoaderData("root");
+
+  console.log(token);
   return (
     <header className={classes.header}>
       <nav>
@@ -39,15 +42,25 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
           <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Authentication
-            </NavLink>
+            {/* PREGUNTA - Por qué se usa el action en el Form */}
+            {token && (
+              <Form action="/logout" method="POST">
+                <button>Logout</button>
+              </Form>
+            )}
           </li>
         </ul>
       </nav>
