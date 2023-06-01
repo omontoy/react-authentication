e4c464cfcf0cc90b3975eff9634c079075a316e6
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-06-01 11:34:54 -0500

    Authentication react project

---
+++ src/pages/Authentication.js
@@ -0,0 +1,47 @@
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export const action = async ({ request }) => {
+  const searchParams = new URL(request.url).searchParams;
+
+  const mode = searchParams.get("mode") || "login";
+  const data = await request.formData();
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode" }, { status: 422 });
+  }
+
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    body: JSON.stringify(authData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user" }, { status: 500 });
+  }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
+  return redirect("/");
+};
